9f950b736914873e01ac22dfb19d5ce3ac6253d9
silviaphungky <silvia.phungky@mandirisekuritas.co.id>
2024-10-22 19:08:36 +0700

    adjust design

--- src/app/(dashboard)/pn/page.tsx
+++ src/app/(dashboard)/pn/page.tsx
@@ -7,7 +7,7 @@ const PNPage = () => {
     <div>
       <div className="flex justify-between">
         <Title title="PN" />
-        <button className="flex gap-2 bg-primary text-white items-center p-2 pr-3 rounded-md text-sm">
+        <button className="flex gap-2 bg-primary text-white items-center p-2 pr-3 rounded-md text-sm hover:opacity-95">
           <IconPlus color="#fff" size={18} />
           New PN
         </button>
