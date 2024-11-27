6e151be25ff9dd1d21df25beff7506241cea4756
silviaphungky <silvia.phungky@mandirisekuritas.co.id>
2024-11-27 18:36:52 +0700

    adjust UI and error message

--- src/app/login/Login.tsx
+++ src/app/login/Login.tsx
@@ -4,19 +4,18 @@ import { useForm, SubmitHandler, Controller, useWatch } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup'
 import * as yup from 'yup'
 import { FormItem, Input } from '@/components'
-// import Image from 'next/image'
+
 import { useRouter } from 'next/navigation'
 import { useMutation } from '@tanstack/react-query'
 import { API_URL } from '@/constants/apiUrl'
 import axios from 'axios'
-import { IconLoading } from '@/icons'
 import IconBatik from '@/icons/IconBatik'
 import Button from '@/components/Button'
 import toast from 'react-hot-toast'
 
 const schema = yup.object().shape({
-  email: yup.string().email().required('Email is required'),
-  password: yup.string().required('Password is required'),
+  email: yup.string().email().required('Email wajib diisi'),
+  password: yup.string().required('Password wajib diisi'),
 })
 
 interface IFormInput {
@@ -41,6 +40,7 @@ const Login = ({
       password: '',
     },
     resolver: yupResolver(schema),
+    reValidateMode: 'onChange',
   })
 
   const email = useWatch({
@@ -77,7 +77,6 @@ const Login = ({
           ...payload,
         }
       )
-
       return response.data.data
     },
   })
@@ -90,12 +89,15 @@ const Login = ({
       const data = response.data
       return data.data
     },
+    onError: (error: any) => {
+      toast.error(error?.response?.data?.message)
+    },
   })
 
   const onSubmit: SubmitHandler<IFormInput> = async (data) => {
     if (isNewAcc) {
       try {
-        await regisNewAccount({
+        const data1 = await regisNewAccount({
           email: data.email,
           password: data.password,
         })
@@ -125,84 +127,95 @@ const Login = ({
         <div className="absolute right-0 top-[3rem]">
           <IconBatik />
         </div>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault()
-            handleSubmit(onSubmit)()
-          }}
-          className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-full max-w-md"
-        >
-          <h2 className="text-center text-2xl mb-4 font-semibold">Sign In</h2>
+        <div className="w-full max-w-md">
+          <div className="flex gap-0 items-center justify-center mb-6">
+            <img src="/logo-kpk.png" width={125} alt="logo" />
+            <div className="font-semibold text-lg ">Parser</div>
+          </div>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault()
+              handleSubmit(onSubmit)()
+            }}
+            className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 "
+          >
+            <h2 className="text-center text-2xl mb-4 font-semibold">Masuk</h2>
 
-          {step === 1 && (
-            <>
-              <div className="mb-4">
-                <Controller
-                  name="email"
-                  control={control}
-                  render={({ field, fieldState: { error } }) => (
-                    <FormItem label="Email" errorMessage={error?.message}>
-                      <Input
-                        type="email"
-                        {...field}
-                        errorMessage={error?.message}
-                        className="w-full"
-                      />
-                    </FormItem>
-                  )}
-                />
-              </div>
-              <Button
-                loading={isChecking}
-                variant="primary"
-                full
-                onClick={() => {
-                  checkAccount(
-                    {
-                      email,
-                    },
-                    {
-                      onSuccess: (data) => {
-                        setIsNewAcc(data.is_need_add_password)
-                        setStep(2)
+            {step === 1 && (
+              <>
+                <div className="mb-4">
+                  <Controller
+                    name="email"
+                    control={control}
+                    render={({ field, fieldState: { error } }) => (
+                      <FormItem label="Email" errorMessage={error?.message}>
+                        <Input
+                          type="email"
+                          {...field}
+                          errorMessage={error?.message}
+                          className="w-full"
+                        />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <Button
+                  loading={isChecking}
+                  variant="primary"
+                  full
+                  onClick={() => {
+                    checkAccount(
+                      {
+                        email,
                       },
-                    }
-                  )
-                }}
-              >
-                Selanjutnya
-              </Button>
-            </>
-          )}
+                      {
+                        onSuccess: (data) => {
+                          setIsNewAcc(data.is_need_add_password)
+                          setStep(2)
+                        },
+                      }
+                    )
+                  }}
+                >
+                  Selanjutnya
+                </Button>
+              </>
+            )}
 
-          {step === 2 && (
-            <>
-              <div className="mb-6">
-                <Controller
-                  name="password"
-                  control={control}
-                  render={({ field, fieldState: { error } }) => (
-                    <FormItem
-                      label={isNewAcc ? 'Atur Password Baru' : 'Password'}
-                      errorMessage={error?.message}
-                    >
-                      <Input
-                        type="password"
-                        {...field}
+            {step === 2 && (
+              <>
+                <div className="mb-6">
+                  <Controller
+                    name="password"
+                    control={control}
+                    render={({ field, fieldState: { error } }) => (
+                      <FormItem
+                        label={isNewAcc ? 'Atur Password Baru' : 'Password'}
                         errorMessage={error?.message}
-                        className="w-full"
-                      />
-                    </FormItem>
-                  )}
-                />
-              </div>
+                      >
+                        <Input
+                          type="password"
+                          {...field}
+                          errorMessage={error?.message}
+                          className="w-full"
+                        />
+                      </FormItem>
+                    )}
+                  />
+                </div>
 
-              <Button type="submit" loading={isPending} variant="primary" full>
-                {isNewAcc ? 'Atur Password dan Masuk' : 'Masuk'}
-              </Button>
-            </>
-          )}
-        </form>
+                <Button
+                  type="submit"
+                  loading={isPending}
+                  variant="primary"
+                  full
+                >
+                  {isNewAcc ? 'Atur Password dan Masuk' : 'Masuk'}
+                </Button>
+              </>
+            )}
+          </form>
+        </div>
       </div>
     </div>
   )
