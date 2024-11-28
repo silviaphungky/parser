e72286ad007a8efe2ae39c5d1bb658ab39db21e7
silviaphungky <silvia.phungky@mandirisekuritas.co.id>
2024-11-28 23:14:24 +0700

    remove useless

--- src/app/(dashboard)/penyelenggara-negara/[id]/components/FamilyList/FamilyList.tsx
+++ src/app/(dashboard)/penyelenggara-negara/[id]/components/FamilyList/FamilyList.tsx
@@ -1,36 +1,6 @@
 'use client'
-import { Card, Pagination, SearchDropdown } from '@/components'
+import { Card } from '@/components'
 import { FamilyTable } from './components'
-import { useState } from 'react'
-import InputDropdown from '@/components/InputDropdown'
-import { IconFilter, IconSort } from '@/icons'
-import DatePickerRange from '@/components/DatePickerRange'
-
-const searchFields = [
-  { label: 'Nama Pemilik Akun', id: 'targetBankName' },
-  { label: 'Remark', id: 'remark' },
-]
-
-const sortOptions = [
-  { label: 'Tanggal Transaksi - Paling baru', id: 'transactionDate' },
-  { label: 'Tanggal Transaksi - Paling lama', id: '-transactionDate' },
-  { label: 'Nominal Transaksi - Paling tinggi', id: 'mutation' },
-  { label: 'Nominal Transaksi - Paling rendah', id: '-mutation' },
-]
-
-const transactionTypeOptions = [
-  { id: '', label: 'Semua Tipe Transaksi' },
-  { id: 'cr', label: 'Credit' },
-  { id: 'db', label: 'Debit' },
-]
-
-const bankOptions = [
-  { value: '', label: 'Semua Akun Bank' },
-  { value: 'BCA1', label: 'BCA - 12345678' },
-  { value: 'BCA1', label: 'BCA - 12345678' },
-  { value: 'BNI1', label: 'BNI - 87654321' },
-  { value: 'BRI1', label: 'BRI - 11223344' },
-]
 
 const FamilyList = ({ token }: { token: string }) => {
   return (
