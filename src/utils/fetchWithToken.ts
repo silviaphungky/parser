a79ca0fcbc164755fdf451e6ed06aadd7b3b9e7e
silviaphungky <silvia.phungky@mandirisekuritas.co.id>
2024-12-08 21:50:26 +0700

    fix baseurl

--- src/utils/fetchWithToken.ts
+++ src/utils/fetchWithToken.ts
@@ -1,9 +1,7 @@
 import { cookies } from 'next/headers'
 
-const baseUrl =
-  'https://499e2567-eab8-4cda-bdb4-d2dd8fb584b8-00-2ns1p7d6pfgj6.pike.replit.dev'
-
 export async function fetchWithToken(
+  baseUrl: string,
   url: string,
   options: RequestInit = {}
 ): Promise<any> {
