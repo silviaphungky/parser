f8be627be57069b6c28a6775fa831d3bf2a682d2
silviaphungky <silvia.phungky@mandirisekuritas.co.id>
2024-12-13 10:44:30 +0700

    add console

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,9 @@ import { NextRequest, NextResponse } from 'next/server'
 
 export function middleware(request: NextRequest): NextResponse {
   const token = request.cookies.get('ACCESS_TOKEN')
+  const xForwardedHost = request.headers.get('x-forwarded-host')
+  const origin = request.headers.get('origin')
+  console.log({ xForwardedHost, origin })
 
   if (!token) {
     const loginUrl = new URL('/login', request.url)
